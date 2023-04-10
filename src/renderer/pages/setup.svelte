87d788ea1c931cbc68cf2e311945ecea86535cd4
mads <47996003+cl0vrfi3ld@users.noreply.github.com>
2023-04-10 17:21:31 -0400

    added some new prettier config, still WIP

--- src/renderer/pages/setup.svelte
+++ src/renderer/pages/setup.svelte
@@ -1,41 +1,40 @@
 <script lang="ts">
-  import TButton from "../components/TButton.svelte";
+	import { getContext, onMount } from 'svelte';
+	import { push } from 'svelte-spa-router';
 
-  import { getContext, onMount } from "svelte";
-  import { user } from "../util/svelte-gun";
-  import type { IpcRenderer } from "electron";
-  import { push } from "svelte-spa-router";
+	import type { IpcRenderer } from 'electron';
 
-  const ipcRenderer: IpcRenderer = window.ipc;
-  let context: AppContext = getContext("AppContext");
+	import TButton from '../components/TButton.svelte';
+	import { user } from '../util/svelte-gun';
 
-  onMount(() => {
-    setTimeout(() => context.setBg(2), 100);
-  });
+	const ipcRenderer: IpcRenderer = window.ipc;
+	let context: AppContext = getContext('AppContext');
+
+	onMount(() => {
+		setTimeout(() => context.setBg(2), 100);
+	});
 </script>
 
 <div class="h-full w-screen">
-  <h1 class=" top-24 absolute text-center text-white text-4xl w-full">
-    where should i put your music?
-  </h1>
-  <div
-    class="flex flex-col items-center justify-center align-middle justify-evenly h-full"
-  >
-    <TButton
-      on:click={async () => {
-        // console.log("clicked");
-        // path = await
-        context.setBg(3);
-        ipcRenderer.invoke("get_save_dir").then((res) => {
-          console.log(res);
-          if (!res) return;
-          //  path = res;
-          user
-            .get("preferences")
-            .get("save_dir")
-            .put(res, () => push("/download/true"));
-        });
-      }}>select folder</TButton
-    >
-  </div>
+	<h1 class=" absolute top-24 w-full text-center text-4xl text-white">
+		where should i put your music?
+	</h1>
+	<div class="flex h-full flex-col items-center justify-center justify-evenly align-middle">
+		<TButton
+			on:click={async () => {
+				// console.log("clicked");
+				// path = await
+				context.setBg(3);
+				ipcRenderer.invoke('get_save_dir').then((res) => {
+					console.log(res);
+					if (!res) return;
+					//  path = res;
+					user
+						.get('preferences')
+						.get('save_dir')
+						.put(res, () => push('/download/true'));
+				});
+			}}>select folder</TButton
+		>
+	</div>
 </div>
