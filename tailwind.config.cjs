621412f2382fbd1f33cedebe72c25832e366d6ee
mads <47996003+cl0vrfi3ld@users.noreply.github.com>
2023-04-28 14:58:10 -0400

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,30 +1,29 @@
 module.exports = {
-  mode: "jit",
-  // you dont need `purge: enabled: production` beca
-  // purge: [],
-  purge: {
-    enabled: false,
-    enabled: !process.env.ROLLUP_WATCH,
-    content: [
-      "./src/renderer/public/index.html",
-      "./src/renderer/App.svelte",
-      "./src/renderer/components/*.svelte",
-      "./src/renderer/pages/*.svelte",
-    ],
-    options: {
-      defaultExtractor: (content) => [
-        ...(content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []),
-        ...(content.match(/(?<=class:)[^=>\/\s]*/g) || []),
-      ],
-    },
-  },
-  darkMode: "class",
-  theme: {
-    extend: {},
-    fontFamily: {
-      sans: ["Nunito", "sans-serif"],
-    },
-  },
-  variants: {},
-  plugins: [],
+	mode: 'jit',
+	// you dont need `purge: enabled: production` beca
+	// purge: [],
+	purge: {
+		enabled: !process.env.ROLLUP_WATCH,
+		content: [
+			'./src/renderer/public/index.html',
+			'./src/renderer/App.svelte',
+			'./src/renderer/components/*.svelte',
+			'./src/renderer/pages/*.svelte'
+		],
+		options: {
+			defaultExtractor: (content) => [
+				...(content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []),
+				...(content.match(/(?<=class:)[^=>\/\s]*/g) || [])
+			]
+		}
+	},
+	darkMode: 'class',
+	theme: {
+		extend: {},
+		fontFamily: {
+			sans: ['Nunito', 'sans-serif']
+		}
+	},
+	variants: {},
+	plugins: []
 };
